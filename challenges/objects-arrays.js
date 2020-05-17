38fc7b27dbccc5e552ee5530d69ba1cf174625fd
NotYourSyn <60495598+NotYourSyn@users.noreply.github.com>
2020-05-17 19:21:24 -0400

    converting functions to arrow functions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -134,7 +134,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = zooAnimals.map(function(value){
+const lowCaseAnimalNames = zooAnimals.map(value => {
   return value.animal_name.toLowerCase();
 })
 console.log(lowCaseAnimalNames);
